0d27abcbd6078e0da1f060eeec8b157606228679
Leandro Ribeiro <bitbucket@leandroribeiro.com>
2017-01-22 16:51:57 -0200

    Incluindo área administrativa

--- Vegetais.Blog.Web/App_Start/RouteConfig.cs
+++ Vegetais.Blog.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,9 @@ namespace Vegetais.Blog.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Vegetais.Blog.Web.Controllers" }
+
             );
         }
     }
--- Vegetais.Blog.Web/BlogModel.edmx.sql
+++ Vegetais.Blog.Web/BlogModel.edmx.sql
@@ -8,7 +8,7 @@
 
 SET QUOTED_IDENTIFIER OFF;
 GO
-USE [master];
+USE [VegetaisBlog];
 GO
 IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
 GO
