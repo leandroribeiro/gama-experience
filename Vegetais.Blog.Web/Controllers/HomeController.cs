bf114e0eefbdc5bba24b9cfeba38aedbaefc34d3
Leandro Ribeiro <bitbucket@leandroribeiro.com>
2017-01-22 15:56:52 -0200

    Movendo código do controller Blog para Index

--- Vegetais.Blog.Web/Controllers/HomeController.cs
+++ Vegetais.Blog.Web/Controllers/HomeController.cs
@@ -3,28 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vegetais.Blog.Web.Models;
 
 namespace Vegetais.Blog.Web.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Blog
         public ActionResult Index()
         {
-            return View();
+            var db = new BlogModelContainer();
+
+            var posts = db.PostSet
+                .ToList()
+                .Select(x => new PostViewModel(x.Titulo, x.Imagem, x.Video, x.Link))
+                .ToList();
+
+            return View(posts);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
