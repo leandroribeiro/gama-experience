893985fb397a12a475c2b1fe8f9d198f815916e7
Leandro Ribeiro <bitbucket@leandroribeiro.com>
2017-01-25 12:06:01 -0200

    Implementando matérias mais vistas dinâmicas

--- Vegetais.Blog.Web/Controllers/BaseController.cs
+++ Vegetais.Blog.Web/Controllers/BaseController.cs
@@ -14,6 +14,18 @@ namespace Vegetais.Blog.Web.Controllers
         public BaseController()
         {
             this.MainLayoutViewModel = new MainLayoutViewModel();
+
+            var db = new BlogModelContainer();
+
+            var artigos = db.ArtigoSet
+                .ToList()
+                //.Select(x => new ArtigoViewModel(x.Titulo, String.Format("{0} {1}", x.Conteudo.Substring(0, 800), "..."), x.Imagem, x.Video, x.Permalink, x.Categoria, x.Autor, x.DataDePublicacao))
+                .Select(x => new ArtigoViewModel(x.Titulo, x.Conteudo, x.Imagem, x.Video, x.Permalink, x.Categoria, x.Autor, x.DataDePublicacao))
+                .OrderByDescending(x => x.DataDePublicacao)
+                .ToList();
+
+            this.MainLayoutViewModel.MateriasMaisVista = artigos;
+
             this.ViewData["MainLayoutViewModel"] = this.MainLayoutViewModel;
         }
     }
