d16609c5c11f1ca65cbacaf7418698bcb7bd5c6a
Leandro Ribeiro <github@leandroribeiro.com>
2017-08-27 22:02:54 -0300

    Scripts auxiliares

---
+++ sql/UPDATE_HoraCadastro.sql
@@ -0,0 +1,15 @@
+/*
+SELECT
+*,
+DATEADD(hour, -2, horacadastro) as HoraCadastroCorrigida
+FROM
+AssociadoSet
+*/
+
+/*
+
+*** SÓ EXECUTAR UMA VEZ !!!!
+
+UPDATE AssociadoSet SET horacadastro=DATEADD(hour, -2, horacadastro)
+
+*/
